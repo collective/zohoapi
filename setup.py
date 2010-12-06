723ddc7d32c4d90ec474487e0f278c3a930847f2
Rok Garbas <rok@garbas.si>
2010-12-06 04:42:16 +0100

    added simplejson as dependency

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'simplejson',
         'poster',
         ],
     test_suite='zohoapi.tests.test_suite',
--- src/zohoapi/__init__.py
+++ src/zohoapi/__init__.py
@@ -1,5 +1,7 @@
 
+from zohoapi import config
 from zohoapi.remote import remote
-from zohoapi import i18n
+from zohoapi.remote import remote_status
 
-__all__ = ['remote', 'i18n']
+
+__all__ = ['config', 'remote', 'remote_status']
